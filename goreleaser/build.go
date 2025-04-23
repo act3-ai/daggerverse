bfec308188beb484a6209fef6a619b36f1c6dcd7
Nathan Joslin <nathan.joslin@udri.udayton.edu>
2025-04-23 14:57:11 -0400

    add goreleaser release subcommand and credential handling

--- goreleaser/build.go
+++ goreleaser/build.go
@@ -8,6 +8,7 @@ import (
 	"github.com/containerd/platforms"
 )
 
+// Release represents the `goreleaser build` command.
 type Build struct {
 	// +private
 	gr *Goreleaser
@@ -25,14 +26,6 @@ func (gr *Goreleaser) Build() *Build {
 	}
 }
 
-// WithConfig loads a .goreleaser.yaml configuration file.
-func (b *Build) WithConfig(config *dagger.File) *Build {
-	cfgPath := "/work/.goreleaser.yaml"
-	b.gr.container = b.gr.container.WithMountedFile(cfgPath, config)
-	b.flags = append(b.flags, "--config", cfgPath)
-	return b
-}
-
 // Build for a specific platform.
 //
 // e.g. `goreleaser build --single-target` with $GOOS, $GOARCH, and $GOARM set appropriately.
@@ -45,7 +38,7 @@ func (b *Build) Platform(
 	p := platforms.MustParse(string(platform))
 	b.flags = append(b.flags, "--single-target")
 
-	return b.gr.container.
+	return b.gr.Container.
 		WithEnvVariable(envGOOS, p.OS).
 		WithEnvVariable(envGOARCH, p.Architecture).
 		With(func(c *dagger.Container) *dagger.Container {
@@ -62,22 +55,38 @@ func (b *Build) Platform(
 //
 // e.g. `goreleaser build`.
 func (b *Build) BuildPlatforms() *dagger.Container {
-	return b.gr.container.WithExec(b.flags)
+	return b.gr.Container.WithExec(b.flags)
+}
+
+// WithConfig loads a .goreleaser.yaml configuration file.
+func (b *Build) WithConfig(config *dagger.File) *Build {
+	cfgPath := "/work/.goreleaser.yaml"
+	b.gr.Container = b.gr.Container.WithMountedFile(cfgPath, config)
+	b.flags = append(b.flags, "--config", cfgPath)
+	return b
 }
 
 // Build an unversioned snapshot, skipping all validations.
 //
-// e.g. `goreleaser build--snapshot`.
+// e.g. `goreleaser build --snapshot`.
 func (b *Build) WithSnapshot() *Build {
 	b.flags = append(b.flags, "--snapshot")
 	return b
 }
 
-// Build an unversioned snapshot, skipping all validations, if the repository is dirty.
+// Automatically sets WithSnapshot if the repository is dirty.
 //
 // e.g. `goreleaser build --auto-snapshot`.
 func (b *Build) WithAutoSnapshot() *Build {
 	b.flags = append(b.flags, "--auto-snapshot")
+	return b
+}
+
+// Removes the 'dist' directory before building.
+//
+// e.g. `goreleaser build --clean`.
+func (b *Build) WithClean() *Build {
+	b.flags = append(b.flags, "--clean")
 	return b
 }
 
@@ -89,14 +98,6 @@ func (b *Build) WithTimeout(
 	duration string,
 ) *Build {
 	b.flags = append(b.flags, "--timeout", duration)
-	return b
-}
-
-// Removes the 'dist' directory before building.
-//
-// e.g. `goreleaser build --clean`
-func (b *Build) WithClean() *Build {
-	b.flags = append(b.flags, "--clean")
 	return b
 }
 
@@ -113,7 +114,7 @@ func (b *Build) WithOptionSkip(
 
 // Tasks to run concurrently (default: number of CPUs).
 //
-// e.g. `goreleaser build --parallelism <n>`
+// e.g. `goreleaser build --parallelism <n>`.
 func (b *Build) WithParallelism(
 	// concurrent tasks
 	n int,
--- goreleaser/main.go
+++ goreleaser/main.go
@@ -1,7 +1,7 @@
 // A module for running the Goreleaser CLI.
 //
 // This module aids in building executables and releasing. The bulk of configuration
-// should be done in .goreleaser.yaml.
+// should be done in a .goreleaser.yaml file.
 
 package main
 
@@ -24,9 +24,12 @@ const (
 	imageGoReleaser = "ghcr.io/goreleaser/goreleaser" // defaults to "latest"
 )
 
+// Goreleaser represents the `goreleaser` command.
 type Goreleaser struct {
 	// +private
-	container *dagger.Container
+	Container *dagger.Container
+	// +private
+	RegistryConfig *dagger.RegistryConfig
 }
 
 func New(
@@ -40,8 +43,10 @@ func New(
 ) *Goreleaser {
 	gr := &Goreleaser{}
 
-	gr.container = dag.Container().
-		From(fmt.Sprintf("%s:%s", imageGoReleaser, version))
+	gr.Container = dag.Container().
+		From(fmt.Sprintf("%s:%s", imageGoReleaser, version)).
+		WithWorkdir("/work/src").
+		WithMountedDirectory("/work/src", Source)
 
 	// inherit from host, overriden by WithEnvVariable
 	val, ok := os.LookupEnv(envGOMAXPROCS)
@@ -60,7 +65,7 @@ func New(
 //
 // This is useful for reusability and readability by not breaking the goreleaser calling chain.
 func (gr *Goreleaser) WithEnvVariable(name, value string, opts ...dagger.ContainerWithEnvVariableOpts) *Goreleaser {
-	gr.container = gr.container.WithEnvVariable(name, value, opts...)
+	gr.Container = gr.Container.WithEnvVariable(name, value, opts...)
 	return gr
 }
 
@@ -68,14 +73,38 @@ func (gr *Goreleaser) WithEnvVariable(name, value string, opts ...dagger.Contain
 //
 // This is useful for reusability and readability by not breaking the goreleaser calling chain.
 func (gr *Goreleaser) WithSecretVariable(name string, secret *dagger.Secret) *Goreleaser {
-	gr.container = gr.container.WithSecretVariable(name, secret)
+	gr.Container = gr.Container.WithSecretVariable(name, secret)
+	return gr
+}
+
+// Add netrc credentials.
+func (gr *Goreleaser) WithNetrc(
+	// NETRC credentials
+	netrc *dagger.Secret,
+) *Goreleaser {
+	gr.Container = gr.Container.WithMountedSecret("/root/.netrc", netrc)
+	return gr
+}
+
+// Add registry credentials.
+func (gr *Goreleaser) WithRegistryAuth(
+	// registry's hostname
+	address string,
+	// username in registry
+	username string,
+	// password or token for registry
+	secret *dagger.Secret,
+) *Goreleaser {
+	gr.RegistryConfig = gr.RegistryConfig.WithRegistryAuth(address, username, secret)
 	return gr
 }
 
 // Run goreleaser.
+//
+// Run is a "catch-all" in case functions are not implemented.
 func (gr *Goreleaser) Run(
 	// arguments and flags, without `goreleaser`.
 	args []string,
 ) *dagger.Container {
-	return gr.container.WithExec(append([]string{"goreleaser"}, args...))
+	return gr.Container.WithExec(append([]string{"goreleaser"}, args...))
 }
